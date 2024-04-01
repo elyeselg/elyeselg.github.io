9278d6fbc80adbe1d4fd6aa07e63e1cba2cc6dfc
p2313439 <elyeselg82@gmail.com>
2024-04-01 17:36:23 +0200

    update 3

--- scriptHome.js
+++ scriptHome.js
@@ -1,16 +1,3 @@
-// Smooth scrolling for anchor links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        const target = document.querySelector(this.getAttribute('href'));
-        target.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    });
-});
-
 // Populate project gallery
 const gallery = document.querySelector('.gallery');
 
