997f9c83309961da58badecbd561994c2f9692ce
p2313439 <elyeselg82@gmail.com>
2024-04-01 17:17:32 +0200

    refonte du site

--- scriptHome.js
+++ scriptHome.js
@@ -10,3 +10,25 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+// Populate project gallery
+const gallery = document.querySelector('.gallery');
+
+const projects = [
+    { name: 'Project 1', image: 'project1.jpg' },
+    { name: 'Project 2', image: 'project2.jpg' },
+    { name: 'Project 3', image: 'project3.jpg' },
+    { name: 'Project 4', image: 'project4.jpg' },
+    { name: 'Project 5', image: 'project5.jpg' },
+    { name: 'Project 6', image: 'project6.jpg' }
+];
+
+projects.forEach(project => {
+    const projectHTML = `
+        <div class="gallery-item">
+            <img src="${project.image}" alt="${project.name}">
+            <h3>${project.name}</h3>
+        </div>
+    `;
+    gallery.innerHTML += projectHTML;
+});
